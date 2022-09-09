9f7e3d6e7fdeef6c9dbe9e88c581eb3994831ca2
Victor Honorato <101150943+vituhonorato@users.noreply.github.com>
2022-09-09 18:26:58 -0300

    building CRUD

--- controllers/series.js
+++ controllers/series.js
@@ -21,11 +21,48 @@ const novaForm = (req,res) => {
     res.render('series/nova')
 }
 
+const excluir = ({Serie},req,res)=> {
+    Serie.remove({
+        _id: req.params.id 
+    }, (err)=> {
+        res.redirect('/series')
+    })
+}
+
+
+const editarProcess =  ({Serie},req,res) => {
+    Serie.findOne({_id: req.params.id}, (err, serie)=>{
+       serie.name=req.body.name
+       serie.status=req.body.status
+       serie.save()
+       res.redirect('/series')
+    })
+ 
+    
+}
+
+
+
+const editarForm = ({Serie},req,res) => {
+    const labels = [
+        {id: 'to-watch', name: 'Para assistir'},
+        {id: 'watched', name: 'Assistido'},
+        {id: 'watching', name:'Assistindo'}
+    ]
+    Serie.findOne({_id: req.params.id}, (err, serie)=>{
+        res.render('series/editar', {serie, labels})
+    })
+   
+}
+
 
 
 
 module.exports = {
     index,
      novaProcess,
-    novaForm
+    novaForm,
+    excluir,
+    editarForm,
+    editarProcess
 }
--- routes/series.js
+++ routes/series.js
@@ -5,9 +5,15 @@ const Serie = require('../models/serie')
 const models = {
     Serie
 }
-
+//READ ROUTE
 router.get('/', seriesController.index.bind(null, models ))
+//CREATE ROUTE
 router.get('/nova', seriesController.novaForm)
 router.post('/nova', seriesController.novaProcess.bind(null, models))
+//REMOVE ROUTE
+router.get('/excluir/:id', seriesController.excluir.bind(null, models) )
+//EDIT ROUTE
+router.get('/editar/:id', seriesController.editarForm.bind(null, models) )
+router.post('/editar/:id', seriesController.editarProcess.bind(null, models) )
 
 module.exports = router
